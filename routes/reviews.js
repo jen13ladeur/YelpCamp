dd06a2649d91e46adfe3276c35160b8069219d7c
Jen <jwong1813@gmail.com>
2021-09-08 09:11:52 -0500

    yelpcamp code

---
+++ routes/reviews.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
+const Campground = require('../models/campground');
+const Review = require('../models/reviews');
+const reviews = require('../controllers/reviews');
+const ExpressError = require('../Utilities/ExpressError');
+const catchAsync = require('../Utilities/catchAsync');
+
+
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
+
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
+
+module.exports = router;
---
+++ seeds/index.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedhelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database Connected');
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '6125294252da711d5c271e3a',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum necessitatibus sapiente facilis cupiditate incidunt beatae in, culpa quos voluptas, quas unde quibusdam quisquam! Quidem cumque perspiciatis eos architecto ipsa quis. Ad vel excepturi porro nihil quibusdam, facere dolores autem illum rem nemo, id quae? Dolor necessitatibus sequi quis obcaecati minus quae, numquam earum facere, fugit explicabo itaque maiores enim quisquam?',
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dchcgzyc4/image/upload/v1630080073/YelpCamp/ttqwojp4ok1i463pykmm.jpg',
+                    filename: 'YelpCamp/ttqwojp4ok1i463pykmm'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dchcgzyc4/image/upload/v1630080073/YelpCamp/gxwcxfa5sngo5dvyypkc.jpg',
+                    filename: 'YelpCamp/gxwcxfa5sngo5dvyypkc'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dchcgzyc4/image/upload/v1630080073/YelpCamp/bpq409kynqfo1slwq7xm.jpg',
+                    filename: 'YelpCamp/bpq409kynqfo1slwq7xm'
+                },
+            ],
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
