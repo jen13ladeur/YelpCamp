2ee1e6a896e0c4058cf31530697e7700ac3e1c19
Jen <jwong1813@gmail.com>
2021-10-12 12:00:19 -0500

    deleted trial code

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -57,15 +57,15 @@ module.exports.renderEditForm = async (req, res) => {
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
     console.log(req.body);
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
-    await campground.save();
+    await campground.save();   
     if (req.body.deleteImages) {
         for (let filename of req.body.deleteImages) {
             await cloudinary.uploader.destroy(filename);
         }
-        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
     }
     req.flash('success', 'Successfully Updated Campground!');
     res.redirect(`/campgrounds/${campground._id}`)
